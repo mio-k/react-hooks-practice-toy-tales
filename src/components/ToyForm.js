2caf9d74c58fb747ac51cf9e6118ebda4365383a
mio kataoka <mio.kanna@gmail.com>
2022-05-19 14:36:58 -0700

    commit tree ef00e03e66501b52a77d20a3e420686a8535c4c2

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({handleAddNewToy}) {
+
+  const [newToyData, setNewToyData] = useState({
+    name:"",
+    image:"",
+    likes: 0
+  })
+
+  function handleChange(e){
+    setNewToyData({
+      ...newToyData,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  function handleFormSubmit(e){
+    e.preventDefault();
+
+    fetch("http://localhost:3001/toys",{
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newToyData),
+    })
+    .then(resp => resp.json())
+    .then(newToy => handleAddNewToy(newToy))
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleFormSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,6 +46,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
@@ -24,6 +54,7 @@ function ToyForm() {
           name="submit"
           value="Create New Toy"
           className="submit"
+          onClick = {handleChange}
         />
       </form>
     </div>
